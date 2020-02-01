df3035624fc1da5cf2d5814a18c68b524bd6f498
Dominique Green <dominiquergreen@yahoo.com>
2020-02-01 22:08:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+ if katz_deli.empty?
+   puts "The line is currently empty."
+ else 
+   current_sentence = "The line is currently:"
+   counter = 1
+   katz_deli.each do |p|
+     current_sentence += " #{counter}. #{p}"
+   counter += 1
+ end
+ puts current_sentence
+ end
+ end
